c4c9d0e7c012d325f2b8b425b59a565b497333ee
oulily <ou.lily890@gmail.com>
2016-08-04 14:57:44 -0700

    Lily

--- Nearby.js
+++ Nearby.js
@@ -7,8 +7,16 @@
 // PLACE DETAILS
 // https://maps.googleapis.com/maps/api/place/details/json?reference=CmRYAAAAciqGsTRX1mXRvuXSH2ErwW-jCINE1aLiwP64MCWDN5vkXvXoQGPKldMfmdGyqWSpm7BEYCgDm-iv7Kc2PF7QA7brMAwBbAcqMr5i1f4PwTpaovIZjysCEZTry8Ez30wpEhCNCXpynextCld2EBsDkRKsGhSLayuRyFsex6JA6NPh9dyupoTH3g&key=AIzaSyCn2FV22kKw7qT7V78tuaG9KiUVV9ilMD4
 
+var latitude = -33.8670522;
+var longitude = 151.1957362;
+var type = restaurant;
+var name = cruise;
+
 $.ajax({
-  dataType: "json",
-  url: https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&name=cruise&key=AIzaSyCn2FV22kKw7qT7V78tuaG9KiUVV9ilMD4,
+  dataType: 'json',
+  url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=latitude,longitude&radius=500&type=type&name=name&key=AIzaSyCn2FV22kKw7qT7V78tuaG9KiUVV9ilMD4',
   success: function(data)
 });
+
+var photoreference = data.results.photos.photo_reference;
+
